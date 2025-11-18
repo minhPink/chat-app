c235775240ec5648734ad1a86735cb6d68d87ca8
xminh <kirito14042003@gmail.com>
2025-11-18 10:32:46 +0700

    sending email welcome

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    const MongoDB_URL = process.env.MONGODB_URL;
+    if (!MongoDB_URL) {
+      throw new Error("MONGODB_URL is not defined in environment variables");
+    }
     const conn = await mongoose.connect(process.env.MONGODB_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
